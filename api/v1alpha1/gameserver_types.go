128359d21e9da4dcde405162ac5e96f9b3fb6665
idebeijer <71566757+idebeijer@users.noreply.github.com>
2024-03-18 21:57:58 +0100

    feat: add gameserver api types

--- api/v1alpha1/gameserver_types.go
+++ api/v1alpha1/gameserver_types.go
@@ -1,21 +1,82 @@
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+type LinuxGSMConfigSource struct {
+	// Config is the LinuxGSM config
+	Config *string `json:"config,omitempty"`
+
+	// ExistingConfigMap is a reference to a ConfigMap containing the LinuxGSM config
+	// If this is set, Config is ignored
+	ExistingConfigMap *v1.ConfigMapKeySelector `json:"existingConfigMap,omitempty"`
+}
+
+type GameConfigSource struct {
+	//// ConfigFileName will be the name of the file to write the config to
+	//ConfigFileName *string `json:"configFileName,omitempty"`
+
+	// Config is the game config
+	Config *string `json:"config,omitempty"`
+
+	// ExistingConfigMap is a reference to a ConfigMap containing the game config
+	// If this is set, Config is ignored
+	ExistingConfigMap *v1.ConfigMapKeySelector `json:"existingConfigMap,omitempty"`
+
+	// MountPath is the path to mount the config file
+	MountPath *string `json:"mountPath,omitempty"`
+}
+
+type GameServerDataPVC struct {
+	// Enabled is a flag to enable or disable the PVC
+	Enabled *bool `json:"enabled,omitempty"`
+
+	// Name is the name of the PVC
+	Name *string `json:"name,omitempty"`
+
+	// Resources represents the minimum resources the volume should have.
+	// More info: https://kubernetes.io/docs/concepts/storage/persistent-volumes#resources
+	Resources v1.ResourceRequirements `json:"resources,omitempty"`
+
+	// StorageClassName is the name of the StorageClass required by the claim.
+	// More info: https://kubernetes.io/docs/concepts/storage/persistent-volumes#class-1
+	StorageClassName *string `json:"storageClassName,omitempty"`
+
+	// Selector is the label selector for the PVC
+	Selector *metav1.LabelSelector `json:"selector,omitempty"`
+}
 
 // GameServerSpec defines the desired state of GameServer
 type GameServerSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of GameServer. Edit gameserver_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Image is the container image to run
+	Image *string `json:"image"`
+
+	// LinuxGSMConfig is the LinuxGSM configuration
+	LinuxGSMConfig *LinuxGSMConfigSource `json:"linuxGSMConfig,omitempty"`
+
+	// GameConfigs is a list of game configs
+	GameConfigs []GameConfigSource `json:"gameConfigs,omitempty"`
+
+	// Ports is a list of ports to expose
+	Ports []v1.ContainerPort `json:"ports,omitempty"`
+
+	// GameServerDataPVC is the spec for the persistent volume claim
+	GameServerDataPVC *GameServerDataPVC `json:"gameServerDataPVC,omitempty"`
+
+	// AdditionalVolumes is a list of additional volumes to mount
+	AdditionalVolumes []v1.Volume `json:"additionalVolumes,omitempty"`
+
+	// AdditionalVolumeMounts is a list of additional volume mounts
+	AdditionalVolumeMounts []v1.VolumeMount `json:"additionalVolumeMounts,omitempty"`
 }
 
 // GameServerStatus defines the observed state of GameServer
 type GameServerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
